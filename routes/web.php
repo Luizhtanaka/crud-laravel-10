a7a60a43307f95f8aa9bfc53bb34dcd55f165c94
Tanaka <luizhenrique.tk@hotmail.com>
2023-09-19 14:13:09 -0400

    add controllers and route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+
+Route::get('/users',[UserController::class,'index'])->name('users.index');
+Route::get('/users/create',[UserController::class,'create'])->name('users.create');
+Route::post('/users',[UserController::class,'store'])->name('users.store');
+Route::get('/users/{user}',[UserController::class,'show'])->name('users.show');
+Route::get('/users/{user}/edit',[UserController::class,'edit'])->name('users.edit');
+Route::put('/users/{user}',[UserController::class,'update'])->name('users.update');
+Route::delete('/users/{user}',[UserController::class, 'destroy'])->name('users.destroy');
+
+
+
+
+
